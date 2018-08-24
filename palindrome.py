ef67c71bf089120ff486e3d50fbed7c98e7289c1
Alexander.Marzan <alexander.marzan@FT-MW4403.local>
2018-08-24 14:01:52 +0100

    Version 2

--- palindrome.py
+++ palindrome.py
@@ -4,20 +4,27 @@ import sys
 
 def isPalindrome(myText):
     array1 = []
-    array2 = []
+    text1 = ""
+    text2 = ""
     string_length = len(myText)
+
+
     if (string_length%2 == 0):
         print ("Even")
         for i in range(0,int(string_length/2)):
-            array1.append(myText[i])
-        array1.sort()
-        print("".join(array1))
+            text1 = myText[i] + text1
+        print("Text1: " + text1)
+
+        text2 = myText[int(string_length/2): int(string_length+1)]
+        print ("Text2: " + text2)
     else:
         print("Odd")
         for i in range(0, int( (string_length - 1) / 2)):
-            array1.append(myText[i])
-        array1.sort()
-        print("".join(array1))
+            text1 = myText[i] + text1
+        print("Text1: " + text1)
+
+        text2 = myText[int(string_length/2)+1: int(string_length+1)]
+        print ("Text2: " + text2)
 
 isPalindrome(sys.argv[1])
 
